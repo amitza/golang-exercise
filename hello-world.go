50e9c71cf71d0483809f02798c08171617d4a9c9
Amit Zafran <aamita00@gmail.com>
2022-06-29 01:01:27 +0300

    first

--- hello-world.go
+++ hello-world.go
@@ -6,6 +6,97 @@ import (
 	"time"
 )
 
+func plus(a int, b int) int {
+	return a + b
+}
+
+func plusPlus(a, b, c int) int {
+	return a + b + c
+}
+
+func vals() (int, int) {
+	return 3, 7
+}
+
+func totalSum(nums ...int) int {
+	sum := 0
+	for _, n := range nums {
+		sum += n
+	}
+	return sum
+}
+
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func zeroval(ival int) {
+	ival = 0
+}
+
+func zeroptr(iptr *int) {
+	*iptr = 0
+}
+
+func fib(n int) int {
+	if n < 2 {
+		return n
+	} else {
+		return fib(n-1) + fib(n-2)
+	}
+}
+
+type person struct {
+	name string
+	age  int
+}
+
+func newPerson(name string) *person {
+	p := person{name: name}
+	p.age = 27
+	return &p
+}
+
+type geometry interface {
+	area() float64
+	perim() float64
+}
+
+type rect struct {
+	width, height float64
+}
+
+func (r rect) area() float64 {
+	return r.width * r.height
+}
+
+func (r rect) perim() float64 {
+	return 2*r.width + 2*r.height
+}
+
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perim())
+}
+
+type base struct {
+	num int
+}
+
+func (b base) describe() string {
+	return fmt.Sprintf("base with num=%v", b.num)
+}
+
+type container struct {
+	base
+	str string
+}
+
 const s string = "constant"
 
 func main() {
@@ -166,6 +257,98 @@ func main() {
 	sliceCopy := make([]string, len(slice))
 	copy(sliceCopy, slice)
 	fmt.Println("cpy:", sliceCopy)
-	
+	l := slice[2:5]
+	fmt.Println("sl1:", l)
+	l = slice[:5]
+	fmt.Println("sl2:", l)
+	l = slice[2:]
+	fmt.Println("sl3:", l)
 
+	tt := []string{"g", "h", "i"}
+	fmt.Println("dcl:", tt)
+	twoDim := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoDim[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoDim[i][j] = i + j
+		}
+	}
+	fmt.Println("2d: ", twoDim)
+
+	mm := make(map[string]int)
+	mm["k1"] = 7
+	mm["k2"] = 13
+	fmt.Println("map: ", mm)
+	v1 := mm["k1"]
+	fmt.Println("v1: ", v1)
+	fmt.Println("len: ", len(mm))
+	delete(mm, "k2")
+	fmt.Println("map: ", mm)
+	_, prs := mm["k2"]
+	fmt.Println("prs:", prs)
+	nn := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("map: ", nn)
+
+	nums := []int{1, 2, 3, 4}
+	sum := 0
+	for i, num := range nums {
+		sum += num
+		fmt.Println("index: ", i)
+	}
+	fmt.Println("sum: ", sum)
+	kvs := map[string]string{"a": "tent", "b": "house"}
+	for k, v := range kvs {
+		fmt.Println("key:", k, "value: ", v)
+	}
+	for i, c := range "go" {
+		fmt.Println(i, c)
+	}
+
+	res := plus(1, 2)
+	fmt.Println("1+2 =", res)
+	res = plusPlus(1, 2, 3)
+	fmt.Println("1+2+3 =", res)
+
+	aa, bb := vals()
+	fmt.Println(aa, bb)
+	_, cc := vals()
+	fmt.Println(cc)
+	fmt.Println("sum is", totalSum(1, 2, 3, 4, 5))
+
+	nextInt := intSeq()
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	fmt.Println(fib(7))
+
+	i = 1
+	zeroval(i) // by value
+	fmt.Println(i)
+	zeroptr(&i) // by reference
+	fmt.Println(i)
+	fmt.Println(&i)
+
+	fmt.Println(person{"bob", 20})
+	fmt.Println(newPerson("bob"))
+
+	rec := rect{width: 10, height: 6}
+	fmt.Println(rec.area(), rec.perim())
+	recPointer := &rec
+	fmt.Println(recPointer.area(), recPointer.perim())
+
+	measure(rec)
+
+	co := container{
+		base: base{num: 1},
+		str:  "some name",
+	}
+
+	type describer interface {
+		describe() string
+	}
+
+	var ddd describer = co
+	fmt.Println("desc: ", ddd.describe())
 }
